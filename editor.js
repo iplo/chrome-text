c4ca1bbbd0a84359a8b3612c2b280f68e1eed492
pigcake999yt <999pigcake@gmail.com>
2017-04-18 09:01:45 +0100

    Add files via upload

--- editor.js
+++ editor.js
@@ -1,4 +1,27 @@
-var filename = "main.js";
+var openFile = function(){
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var contents = event.target.result;
+        console.log("File contents: " + contents);
+    };
+
+    reader.onerror = function(event) {
+        console.error("File could not be read! Code " + event.target.error.code);
+    };
+
+    console.log(reader.readAsText);
+
+    return reader;
+};
+
+$(document).ready(function(){
+    files = false;
+    $(".nofile").show();
+});
+
+window.onload = function(){
+    loadFile("index.html", "./");
+};
 
 function toMode(fn){
     var filen = fn;
@@ -14,23 +37,50 @@ function toMode(fn){
 
     if(extname == "js"){
         mode = "javascript";
-    };
+    } else if(extname == "html" || extname == "htm"){
+        mode = "htmlmixed";
+    } else if(extname == "css"){
+        mode = "css";
+    } else if(extname == "scss"){
+        mode = "text/x-scss";
+    } else if(extname == "txt"){
+        mode = "plain";
+    } else if(extname == "py"){
+        mode = "python";
+    } else if(extname == "php"){
+        mode = "php";
+    } else if(extname == "sas"){
+        mode = "sas";
+    } else if(extname == "sass"){
+        mode = "sass";
+    } else {
+        mode = "plain";
+    }
 
-    // console.log(mode)
+    console.log(mode);
 
     return mode;
 }
 
-var modeX = toMode(filename);
-var value = "console.log('hi');";
-var editor = CodeMirror(document.body, {
-    value: value,
-    lineNumbers: true,
-    mode: modeX,
-    keyMap: "sublime",
-    autoCloseBrackets: true,
-    matchBrackets: true,
-    showCursorWhenSelecting: true,
-    theme: "monokai",
-    tabSize: 4
-});
+theme = "monokai";
+
+function loadFile(filename, dir){
+    $(".fn").val(filename);
+    $(".nofile").hide();
+    files = true;
+    var modeX = toMode(filename);
+    var value = "<content></content>";
+    var editor = CodeMirror(document.body, {
+        showHint: true,
+        value: value,
+        lineNumbers: true,
+        mode: modeX,
+        keyMap: "sublime",
+        autoCloseBrackets: true,
+        matchBrackets: true,
+        showCursorWhenSelecting: true,
+        theme: theme,
+        tabSize: 4,
+        smartIndent: true
+    });
+}
